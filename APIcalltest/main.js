4792715f8a85bd422f14b78e4718fe28b496442e
Holly Heredia <hollyheredia@gmail.com>
2021-07-22 20:49:45 -0700

    Started outline for D3 app

--- APIcalltest/main.js
+++ APIcalltest/main.js
@@ -1,6 +1,6 @@
 // you will also have to setup the referring domains on your marvel developer portal
 var PRIV_KEY = "2594b3a3e1eb34f28ff5d1d7e8b42365f2c3f4bf";
-var PUBLIC_KEY = "a65e55ecaa3e74606bacb7f25819f9be";
+var PUBLIC_KEY = "975a48fc3bc02d2c70d24be13fa144965d627d30";
 
 function getMarvelResponse() {
 
@@ -9,17 +9,16 @@ function getMarvelResponse() {
   var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
   
   // the api deals a lot in ids rather than just the strings you want to use
-  var characterId = '1009718'; // wolverine                                                                             
+  // var characterId = '1009718'; // wolverine                                                                             
 
 
-  var url = 'http://gateway.marvel.com:80/v1/public/comics';
+  var url = 'http://gateway.marvel.com:80/GET/v1/public/characters/';
 
   console.log(url);
   $.getJSON(url, {
     ts: ts,
     apikey: PUBLIC_KEY,
-    hash: hash,
-    characters: characterId
+    hash: hash
     })
     .done(function(data) {
       // sort of a long dump you will need to sort through
