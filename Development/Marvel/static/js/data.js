8eb5a7e89c172fe94e64064bf5a3ffe1d0558ace
Holly Heredia <hollyheredia@gmail.com>
2021-07-29 19:55:52 -0700

    Added some files back from Git Station.

---
+++ Development/Marvel/static/js/data.js
@@ -0,0 +1,5 @@
+var good_characters = $.csv.toObjects(data/good_characters.csv)
+
+var bad_characters = $.csv.toObjects(data/bad_characters.csv)
+
+var neutral_characters = $.csv.toObjects(data/neutral_characters.csv)
---
+++ Development/Marvel/static/js/dropdown.js
@@ -0,0 +1,45 @@
+function init() {
+  var data = [{
+    values: us,
+    labels: labels,
+    type: "pie"
+  }];
+
+  var layout = {
+    height: 600,
+    width: 800
+  };
+
+  Plotly.newPlot("pie", data, layout);
+}
+
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", optionChanged);
+
+// Function called by DOM changes
+function optionChanged() {
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  var dataset = dropdownMenu.property("value");
+  // Initialize an empty array for the country's data
+  var data = [];
+
+  if (dataset == 'Good') {
+      data = good_characters;
+  }
+  else if (dataset == 'uk') {
+      data = uk;
+  }
+  else if (dataset == 'canada') {
+      data = canada;
+  }
+  // Call function to update the chart
+  updatePlotly(data);
+}
+
+// Update the restyled plot's values
+function updatePlotly(newdata) {
+  Plotly.restyle("pie", "values", [newdata]);
+}
+
+init();
